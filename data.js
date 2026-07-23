3595b0f26b97e4710dafba58d981448943258d1b
whoisbagaskr <ba6422977@gmail.com>
2026-07-23 11:01:08 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const portfolioData = {
+  profile: {
+    name: "Raden Arya Bagaskara",
+    title: "Cyber Security & OSINT Enthusiast",
+    bio: "Passionate about Open Source Intelligence (OSINT), Network Security, and Ethical Hacking.",
+    socials: {
+      instagram: "https://instagram.com/whoisbagaskr",
+      github: "https://github.com/whoisbagaskr"
+    }
+  },
+  projects: [
+    {
+      title: "Project Portofolio",
+      description: "Eksperimen portofolio pribadi pertama menggunakan HTML, CSS, dan JS.",
+      tags: ["HTML", "CSS", "JS"],
+      link: "#"
+    }
+  ]
+};
